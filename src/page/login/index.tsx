25ed348c30bc2b99335291a9e7232be8fad4fd9c
xueming <810505339@qq.com>
2023-09-07 09:36:10 +0800

    add login

---
+++ src/page/login/index.tsx
@@ -0,0 +1,11 @@
+import {Layout, Text} from '@ui-kitten/components';
+
+const Login = () => {
+  return (
+    <Layout>
+      <Text>1</Text>
+    </Layout>
+  );
+};
+
+export default Login;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,14 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import HomeScreen from '@page/home/index';
+import HomeScreen from '@page/home';
 import useSysLanguage from 'hooks/useSysLanguage';
+import LoginScreen from '@page/login';
 
 const Stack = createNativeStackNavigator();
 
 const HomeNavigator = () => (
-  <Stack.Navigator screenOptions={{headerShown: false}}>
+  <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false}}>
+    <Stack.Screen name="Login" component={LoginScreen} />
     <Stack.Screen name="Home" component={HomeScreen} />
   </Stack.Navigator>
 );

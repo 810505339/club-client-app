63de4c2dc58a62bfc42564f15345b1b6d305e078
xue ming <810505339@qq.com>
2023-09-25 22:29:16 +0800

    edit

---
+++ src/pages/demoScreen/index.tsx
@@ -0,0 +1,15 @@
+import {View, Text} from 'react-native';
+import {Appbar} from 'react-native-paper';
+
+const demoScreen = () => {
+  return (
+    <View>
+      <Appbar.Header>
+        <Appbar.Content title="Title" subtitle={'Subtitle'} />
+        <Appbar.Action icon="magnify" onPress={() => {}} />
+        <Appbar.Action icon="dots-horizontal" onPress={() => {}} />
+      </Appbar.Header>
+    </View>
+  );
+};
+export default demoScreen;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,14 @@
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
 import useSysLanguage from 'hooks/useSysLanguage';
 import {getGenericPassword} from 'react-native-keychain';
 import {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {adaptNavigationTheme} from 'react-native-paper';
 import HomeTabs from './HomeTabs';
 import LoginScreen from '@pages/LoginScreen';
+import DemoScreen from '@pages/demoScreen';
 const Stack = createNativeStackNavigator();
-
+const {DarkTheme} = adaptNavigationTheme({reactNavigationDark: DefaultTheme});
 const AppNavigator = () => {
   useSysLanguage();
 
@@ -26,12 +28,13 @@ const AppNavigator = () => {
   }, []);
 
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={DarkTheme}>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName="HomeTabs"
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="HomeTabs" component={HomeTabs} />
         <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="DemoScreen" component={DemoScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
